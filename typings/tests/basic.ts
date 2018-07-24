ff6e4fc77db33c99f82cdeab66785658b7911449
Kevin Busch <kevin@visimpact.com>
2018-07-23 22:45:40 -0400

    Updated TS typings file to reflect appropriate function export formatting.  Updated TSC tests

--- typings/tests/basic.ts
+++ typings/tests/basic.ts
@@ -1,21 +1,19 @@
 import * as http from "http";
 import * as next from "next";
-import Routes from "../..";
+import * as routes from "../..";
 
-const routes = new Routes();
-
-routes
-  .add("login")
-  .add("home", "/", "index")
-  .add("/settings", "/users/:id/settings")
-  .add("/example", "example")
-  .add({ name: "objectstyle", pattern: "/cool", page: "cool" });
+const nextroutes = routes()
+.add("login")
+.add("home", "/", "index")
+.add("/settings", "/users/:id/settings")
+.add("/example", "example")
+.add({ name: "objectstyle", pattern: "/cool", page: "cool" });
 
 export const createServer = () => {
   const app = next({ dev: true });
   return app.prepare().then(() => {
-    return http.createServer(routes.getRequestHandler(app));
+    return http.createServer(nextroutes.getRequestHandler(app));
   });
 };
 
-export default routes;
+export default nextroutes;
