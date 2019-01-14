85e69e053e8967cff6504180c0a54e2f7e49ad49
Spencer Elliott <me@elliottsj.com>
2019-01-14 14:41:36 -0500

    Configure Jest + Babel to compile TypeScript tests

--- .babelrc.js
+++ .babelrc.js
@@ -2,9 +2,18 @@
 // and to compile sources (including TypeScript) for execution in Jest's environment.
 // See https://babeljs.io/docs/en/config-files#file-relative-configuration
 
+const presets = [
+  require('@babel/preset-env'),
+  require('next/babel')
+]
+
+// Only in the Jest environment, compile TypeScript sources
+if (process.env.NODE_ENV === 'test') {
+  presets.push(
+    require('@zeit/next-typescript/babel')
+  )
+}
+
 module.exports = {
-  presets: [
-    require('@babel/preset-env'),
-    require('next/babel')
-  ]
+  presets
 }
--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,17 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+
   // The test environment that will be used for testing
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+
+  // The pattern Jest uses to detect test files
+  testRegex: '(/__tests__/.*|(.|/)(test|spec)).(ts|tsx|js)$',
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.jsx?$': require.resolve('./jest/babelTransformer')
+    '^.+\\.(ts|tsx|js)$': require.resolve('./jest/babelTransformer')
   }
 }
