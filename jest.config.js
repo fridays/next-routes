9a92d19af00d86b5416daf4ca70bf880115f7c6d
Spencer Elliott <me@elliottsj.com>
2019-01-14 14:04:37 -0500

    Move Jest config to jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node'
+}
