ae907b583f61b560c25dcce10b1bb7938214035b
Injung Chung <mu29@yeoubi.net>
2019-02-08 16:09:01 +0900

    Added missing types

--- typings/next-routes.d.ts
+++ typings/next-routes.d.ts
@@ -3,6 +3,7 @@ import { Server } from "next";
 import { ComponentType } from "react";
 import { LinkState } from "next/link";
 import { SingletonRouter, EventChangeOptions } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 
 export type HTTPHandler = (
   request: IncomingMessage,
@@ -44,11 +45,32 @@ export interface Registry {
   Router: Router;
 }
 
+export interface Route {
+  match(path: string): any;
+  valuesToParams(values: any[]): any;
+  getHref(params: RouteParams): string;
+  getAs(params: RouteParams): string;
+  getUrls(params: RouteParams): { as: string; href: string; };
+  page: string;
+  name: string;
+}
+
 export default class Routes implements Registry {
   getRequestHandler(app: Server, custom?: HTTPHandler): HTTPHandler;
   add(name: string, pattern?: string, page?: string): this;
   add(pattern: string, page: string): this;
   add(options: { name: string; pattern?: string; page?: string }): this;
+  findByName(name: string): Route;
+  match(url: string): {
+    route: Route;
+    params: any;
+    query: ParsedUrlQuery;
+  };
+  findAndGetUrls(nameOrUrl: string, params: RouteParams): {
+    route: Route;
+    urls: { as: string; href: string };
+    byName?: boolean;
+  };
   Link: ComponentType<LinkProps>;
   Router: Router;
 }
